97b9989112dfc0fce24b02dfb933faeaff8fc06d
liujerry57 <liu.jerry57@gmail.com>
2021-02-13 22:04:17 -0800

    removed a command

--- commands/whosabitch.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'whosabitch',
-	description: 'no',
-	execute(message, args) {
-		const user = message.author;
-		console.log('bitch is running');
-		message.channel.send('<@' + message.author.id + '> is a bitch');
-	},
-};
