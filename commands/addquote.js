5652b4829e3b9eabd95c3ad48df02951b8e64dd6
liujerry57 <liu.jerry57@gmail.com>
2021-03-24 16:45:51 -0700

    addquote now uses messages time instead of current

--- commands/addquote.js
+++ commands/addquote.js
@@ -5,23 +5,30 @@ const quoteFormat = /"(.)*"(\s)*-(\s)*(.)*/; // Matches "<quote>" - <author>
 module.exports = {
 	name: 'addquote',
 	description: 'Adds a quote to database',
-	execute(message) {
+	async execute(message) {
 		if (message.content.match(quoteFormat)) {
 			var fullQuote = message.content.trim().split(/-/);
 			var quote = fullQuote[0].trim();
-			if (quote[0] == "\"" && quote[quote.length - 1] == "\"") {
-				quote = quote.substring(1, quote.length - 1);
+			
+			var temp = fullQuote[1].trim().split(/\s+/).join(" ")
+			var speaker = (temp == "") ? "unknown" : temp; // Format author in case excess whitespace
+
+			var time = moment(message.createdAt).format('MMM Do YY, h:mm a');
+
+			var count = await Quote.find({ 
+				speaker: speaker, 
+				quote: quote, 
+				dateEntered: time
+			}).count();
+			if (count == 0) {
+				var quoteModel = new Quote({
+					speaker: speaker,
+					quote: quote,
+					dateEntered: time
+				}); 
+				quoteModel.save();
 			}
-
-			var temp = fullQuote[1].trim().split(/\s+/).join(" ");
-			var speaker = (temp == "") ? "unknown" : temp;
-
-			var quoteModel = new Quote({
-				speaker: speaker,
-				quote: quote,
-				dateEntered: moment().format('MMM Do YY, h:mm a')
-			}); 
-			quoteModel.save();
+			
 		}
 		
 
